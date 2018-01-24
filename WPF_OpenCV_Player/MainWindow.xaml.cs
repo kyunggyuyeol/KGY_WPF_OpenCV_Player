e35d85d7071ec679da7fa929a253644c51a7fdb5
경규열 <p82468@paran.com>
2018-01-24 13:24:30 +0900

    imshow

--- WPF_OpenCV_Player/MainWindow.xaml.cs
+++ WPF_OpenCV_Player/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,11 +22,65 @@ namespace WPF_OpenCV_Player
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void player()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string fullpath = System.IO.Path.Combine(path, "cheerleading_0001.mp4");
+            VideoCapture video = VideoCapture.FromFile(fullpath);
+
+            if (!video.IsOpened())
+            {
+                MessageBox.Show("not open");
+                return;
+            }
+
+            Mat frame = new Mat();
+
+            video.Set(CaptureProperty.FrameWidth, video.FrameWidth);
+            video.Set(CaptureProperty.FrameHeight, video.FrameHeight);
+            double fps = video.Get(CaptureProperty.Fps);
+            
+            while (true)
+            {
+                if (video.Read(frame))
+                {
+                    if (frame.Width == 0 && frame.Height == 0)
+                        break;
+
+                    Cv2.ImShow("00", frame);
+                    WriteableBitmap wb = new WriteableBitmap(video.FrameWidth, video.FrameHeight, 96, 96, PixelFormats.Bgr24, null);
+                    WriteableBitmapConverter.ToWriteableBitmap(frame, wb);
+
+                    Dispatcher.Invoke(new Action(delegate ()
+                    {
+                        img_player.Source = wb;
+                    }));
+
+                    if (Cv2.WaitKey(10) == 27)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void btn_player_Click(object sender, RoutedEventArgs e)
+        {
+            //Task t1 = new Task(new Action(player));
+            //t1.Start();
+            player();
         }
     }
 }
